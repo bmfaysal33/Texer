291097e4c6bb0844917b85e4bdbf828d0d772bd6
BM Faysal <bmfaysal33@gmail.com>
2025-09-04 15:16:38 +0600

    updated some UI and made mobile responsive

--- js/index.js
+++ js/index.js
@@ -16,9 +16,9 @@ document.getElementById('btn-post').addEventListener('click', function (event) {
 
    
    childrens.innerHTML = `
-    <div id="series" class="border-4 w-3/4 mx-auto rounded-2xl my-5 p-5"
+    <div id="series" class="border-2 md:border-4 w-3/4 mx-auto rounded-2xl my-2 md:my-5 p-2 md:p-5"
     >
-    <h1 class="text-2xl text-black font-bold">  ${inputValue} </h1>
+    <h1 class=" md:text-2xl text-black font-bold">  ${inputValue} </h1>
     </div>
     `;
 
